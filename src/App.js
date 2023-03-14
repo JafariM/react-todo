5c39d338382ce0c5e3975e5b8aafaf5be602cd92
Masooma <masuma.jafari@gmail.com>
2023-03-14 13:20:20 -0700

    make a ul in app.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: 'Complete assignment'
+  },
+  {
+    id: 2,
+    title: 'praper the lunch'
+  },
+  {
+    id: 3,
+    title: 'pick up my son from school'
+  }
+];
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function (item) {
+          return (<li key={item.id}>{item.title}</li>)
+        })}
+      </ul>
     </div>
   );
 }
