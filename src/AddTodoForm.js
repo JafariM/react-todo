89d975f126f51c1d59c4b45fa0279c3dc363eef2
Masooma <masuma.jafari@gmail.com>
2023-05-18 13:26:45 -0700

    lising, adding and deleteing todo itmes from airtable

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -2,9 +2,8 @@ import React from "react";
 import InputWithLabel from "./InputWithLabel";
 
 function AddTodoForm({ onAddTodo }) {
-
   //state of input form for adding todo title
-  const [todoTitle, setTodoTitle] = React.useState('');
+  const [todoTitle, setTodoTitle] = React.useState("");
 
   function handleTitleChange(event) {
     const newTodoTitle = event.target.value;
@@ -15,12 +14,9 @@ function AddTodoForm({ onAddTodo }) {
   function handleAddTodo(event) {
     event.preventDefault();
     //pass the value of input form to App.js
-    onAddTodo({
-      title: todoTitle,
-      id: Date.now()
-    });
+    onAddTodo(todoTitle);
     //empty input field
-    setTodoTitle('');
+    setTodoTitle("");
   }
 
   return (
@@ -31,10 +27,12 @@ function AddTodoForm({ onAddTodo }) {
       >
         <strong>Title</strong>
       </InputWithLabel>
-      <button type="submit" >Add</button>
-      <p>This is new to do list item:<strong>{todoTitle}</strong></p>
+      <button type="submit">Add</button>
+      <p>
+        This is new to do list item:<strong>{todoTitle}</strong>
+      </p>
     </form>
-  )
+  );
 }
 
 export default AddTodoForm;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
-
+import React from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 //NO USE
 //define a custom hook to syn todolist and browser local storage
@@ -19,46 +18,123 @@ import AddTodoForm from './AddTodoForm';
 function App() {
   const [todoList, setTodoList] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  //url to Airtable
+  const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/`;
 
-  React.useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(
-        // read initial value of state hook form local storage
-        () => resolve({ data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')) } }),
-        2000)
-    }).then((result) => {
-      setTodoList(result.data.todoList);
+  //fetching from Air table
+  const fetchData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
+    };
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        const message = `Error: ${response.status}`;
+        throw new Error(message);
+      }
+
+      const data = await response.json();
+
+      const todos = data.records.map((todo) => {
+        const newTodo = {
+          id: todo.id,
+          title: todo.fields.title,
+        };
+        return newTodo;
+      });
+
+      setTodoList(todos);
       setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
     }
-    )
+  };
+  //add new todo to air table
+  const addTodo = async (title) => {
+    try {
+      const airtableData = {
+        fields: {
+          title: title,
+        },
+      };
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+        },
+        body: JSON.stringify(airtableData),
+      });
+
+      if (!response.ok) {
+        const message = `Error has ocurred:
+                             ${response.status}`;
+        throw new Error(message);
+      }
+
+      const dataResponse = await response.json();
+      console.log(dataResponse);
+      const fetchNewTodo = {
+        id: dataResponse.id,
+        title: dataResponse.fields.title,
+      };
+      setTodoList([...todoList, fetchNewTodo]);
+    } catch (error) {
+      console.log(error.message);
+      return null;
+    }
+  };
+  //delete todo from airtable
+  const removeTodo = async (id) => {
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
+    };
+
+    try {
+      const response = await fetch(`${url}${id}`, options);
+      if (!response.ok) {
+        const message = `Error has occurred:
+        ${response.status}`;
+        throw new Error(message);
+      }
+
+      const newTodoList = todoList.filter((item) => item.id !== id);
+      setTodoList(newTodoList);
+    } catch (error) {
+      console.log(error.message);
+      return null;
+    }
+  };
+  React.useEffect(() => {
+    fetchData();
   }, []);
+
   //set a side effect hook to save the todolist in browser's local storage
   React.useEffect(() => {
     if (!isLoading) {
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
     }
-  }, ['savedTodoList', JSON.stringify(todoList)]); // with stringify we can read the values inside todolist array
+  }, ["savedTodoList", JSON.stringify(todoList)]); // with stringify we can read the values inside todolist array
 
   // const [todoList, setTodoList] = useSemiPersistentState();
 
-  // add new todo to the list and update todolist
-  function addTodo(newTodo) {
-    setTodoList([...todoList, newTodo])
-  }
-
-  //remove an item from todoList
-  function removeTodo(id) {
-    const updatedTodo = todoList.filter(
-      (todoItem) => todoItem.id !== id   //remove item if does not match the id
-    )
-    setTodoList(updatedTodo);
-
-  }
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? <p>Loading</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
     </>
   );
 }
