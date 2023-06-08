74532c6d3bd8bb7a029ae8d3599685cdd74cefa9
Masooma <masuma.jafari@gmail.com>
2023-06-07 23:15:12 -0700

    Adding a css module and use it in TodoListItem component

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,14 +1,16 @@
 import React from "react";
-
+import style from "./TodoListItem.module.css";
 
 function TodoListItem({ todo, onRemoveTodo, id }) {
   return (
-    <li>
+    <li className={style.listItem}>
       {todo}
       &nbsp;
-      <button type="button" onClick={() => onRemoveTodo(id)}>Remove</button>
+      <button type="button" onClick={() => onRemoveTodo(id)}>
+        Remove
+      </button>
     </li>
-  )
+  );
 }
 
 export default TodoListItem;
