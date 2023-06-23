e68f8dd5b81dd15825bd732488ad9ac84cbe71f3
Masooma <masuma.jafari@gmail.com>
2023-06-23 11:43:34 -0700

    Improve Project Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import TodoList from "./TodoList";
-import AddTodoForm from "./AddTodoForm";
+import TodoList from "./components/TodoList";
+import AddTodoForm from "./components/AddTodoForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import style from "./TodoListItem.module.css";
-import backroundImage from "./img/day-night.jpg";
 
 //NO USE
 //define a custom hook to syn todolist and browser local storage
